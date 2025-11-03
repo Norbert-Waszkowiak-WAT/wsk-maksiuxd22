ad9b8432fe44627c1855e7328741c00c5e11ee63
maksiuxd22 <maksymiliancieslak15@wp.pl>
2025-11-03 10:08:38 +0000

    zrobione 2

--- ex01.cpp
+++ ex01.cpp
@@ -1,13 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
+ 
+int sum_array(int*a,int size){
+    int suma=0;
+    for(int*p=a;p<a+size;p++){
+        suma=suma+*p;
+    }
+    return suma;
+}
+ 
 int main(){
-    int x=22;
-    int* p=&x;
-    cout<<p<<endl;
-    cout<<*p<<endl;
-    x=*p=6;
-    cout<<x<<endl;
+    int tab[5]={2,2,6,2,7};
+    int bab[3]={5,10,15};
+    cout<<sum_array(tab,5)<<endl;
+    cout<<sum_array(bab,3)<<endl;
+ 
     return 0;
-
 }
